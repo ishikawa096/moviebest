d81ed59c5dc4f409836fc34effa1c0ab51fb2394
ishikawa096 <ishikawa096@gmail.com>
2022-07-15 16:41:35 +0900

    model validates設定

--- backend/app/models/list.rb
+++ backend/app/models/list.rb
@@ -3,6 +3,17 @@ class List < ApplicationRecord
   has_many :movies, dependent: :destroy
   validates_associated :movies
 
+  validates :comment, length: { maximum: 1000 }
+  validates :numbered, inclusion: { in: [true, false] }
+  validates :theme_id, presence: true, length: { maximum: 100 }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :number_of_movies_match_theme_capacity
+
+  def number_of_movies_match_theme_capacity
+    if theme.capacity != movies.size
+      errors.add(:movies, "を#{theme.capacity}つ入力してください")
+    end
+  end
+
   class << self
     def create!(params)
       list = List.new(
