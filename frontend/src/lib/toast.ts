37daad4e013f663a2d1e5860deb217a1b8d1cbaa
ishikawa096 <ishikawa096@gmail.com>
2022-07-15 16:47:59 +0900

    react-toastify install

---
+++ frontend/src/lib/toast.ts
@@ -0,0 +1,29 @@
+// import 'react-toastify/dist/ReactToastify.css'
+import { toast, Flip } from 'react-toastify'
+
+const defaults = {
+  position: 'bottom-center',
+  autoClose: 5000,
+  hideProgressBar: true,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  transition: Flip,
+}
+
+export const toastSuccess = (message: string) => {
+  toast.success(message, Object.assign(defaults))
+}
+
+export const toastInfo = (message: string) => {
+  toast.info(message, Object.assign(defaults))
+}
+
+export const toastWarn = (message: string) => {
+  toast.warn(message, Object.assign(defaults))
+}
+
+export const toastError = (message: string) => {
+  toast.error(message, Object.assign(defaults))
+}
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import '/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Error from 'next/error'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Layout from 'components/layout/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (pageProps.error) {
+    return <Error statusCode={pageProps.error.statusCode} title={pageProps.error.message} />
+  }
   return (
     <>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <ToastContainer />
     </>
   )
 }
