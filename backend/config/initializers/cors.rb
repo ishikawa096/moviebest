e711a5501e4b539a0e214e84d6e57958430f36bd
ishikawa096 <ishikawa096@gmail.com>
2022-07-22 04:26:48 +0900

    devise設定、userモデル作成

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -14,6 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
     resource '*',
       headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :lists, only: %i[index show create]
       resources :themes, only: %i[index show create]
+      resources :user, only: %i[show]
       get :health_checks, to: 'health_checks#index'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+      namespace :auth do
+        resources :sessions, only: %i[index]
       end
     end
   end
