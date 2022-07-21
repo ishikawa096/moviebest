376a9778836e4a22d9f5b04ef99d4a88768e518c
ishikawa096 <ishikawa096@gmail.com>
2022-07-22 04:36:44 +0900

    改行追加

--- backend/app/models/movie.rb
+++ backend/app/models/movie.rb
@@ -2,5 +2,6 @@ class Movie < ApplicationRecord
   belongs_to :list
 
   validates :title, presence: true, length: { maximum: 100 }
-  validates :position, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :position, presence: true,
+                       numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- backend/app/models/theme.rb
+++ backend/app/models/theme.rb
@@ -2,5 +2,6 @@ class Theme < ApplicationRecord
   has_many :lists, dependent: :restrict_with_error
 
   validates :title, presence: true, length: { maximum: 100 }
-  validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :capacity, presence: true,
+                       numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
