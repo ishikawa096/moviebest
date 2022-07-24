f8fea55b8da87ad96b480be4682ac71aa1226c21
ishikawa096 <ishikawa096@gmail.com>
2022-07-24 10:47:48 +0900

    登録用のtype定義を作成したため差し替え

--- frontend/src/lib/validates.ts
+++ frontend/src/lib/validates.ts
@@ -1,17 +1,17 @@
-import type { Theme, List, Movie } from "interfaces/interface";
+import type { createListParams, CreateThemeParams } from "interfaces/interface";
 
-export const validateTheme = (theme: Theme) => {
-  const errors: { [K in keyof Theme]?: string } = {}
+export const validateTheme = (theme: CreateThemeParams) => {
+  const errors: { [K in keyof CreateThemeParams]?: string } = {}
   if (theme.title === '') {
     errors.title = 'お題を入力してください'
   }
   return errors
 }
 
-export const validateList = (list: List) => {
+export const validateList = (list: createListParams) => {
   // type Movies = 'movies'
   // const errors: { [K in keyof Omit<List, Movies>]?: string } & { movies?: Array<string> } = {}
-  const errors: { [K in keyof List]?: string } = {}
+  const errors: { [K in keyof createListParams]?: string } = {}
   list.movies?.map((m) => {
     if (m.title === '') {
     errors.movies = 'タイトルを入力してください'
