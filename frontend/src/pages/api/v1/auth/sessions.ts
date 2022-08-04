782ced01f826915b7cc8bb25ac0f4621a8cffaef
ishikawa096 <ishikawa096@gmail.com>
2022-08-04 18:07:44 +0900

    認証ヘッダーを切り出し

--- frontend/src/pages/api/v1/auth/sessions.ts
+++ frontend/src/pages/api/v1/auth/sessions.ts
@@ -1,16 +1,13 @@
+import { authHeaders } from 'lib/api/authHelper'
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 import nookies from 'nookies'
 import { client } from '../client'
 
 export const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const cookies = nookies.get({ req })
+  const cookies = nookies.get({ req })
   try {
     const response = await client.get(`/auth/sessions`, {
-      headers: {
-        'access-token': cookies._access_token,
-        client:cookies. _client,
-        uid: cookies._uid,
-      },
+      headers: authHeaders(cookies),
     })
     if (response.status === 200) {
       res.status(200).json(response.data)
