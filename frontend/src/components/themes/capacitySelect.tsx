65621eca0a2b8c9a3fc0496da7ff05166fbd0f50
ishikawa096 <ishikawa096@gmail.com>
2022-08-09 21:33:58 +0900

    繰り返しの記述を省略

--- frontend/src/components/themes/capacitySelect.tsx
+++ frontend/src/components/themes/capacitySelect.tsx
@@ -10,38 +10,21 @@ interface Props {
 
 const CapacitySelect = ({ onChange, theme, capacities, formErrors }: Props) => (
   <>
-    {!formErrors.capacity ? (
-      <>
-        <select
-          name='capacity'
-          onChange={onChange}
-          value={theme.capacity}
-          className='text-xl sm:text-2xl lg:text-3xl bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-        >
-          {capacities.map((n) => (
-            <option value={n} key={n}>
-              {n}
-            </option>
-          ))}
-        </select>
-      </>
-    ) : (
-      <>
-        <select
-          name='capacity'
-          onChange={onChange}
-          value={theme.capacity}
-          className='text-xl sm:text-2xl lg:text-3xl bg-gray-50 border-2 border-red-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
-        >
-          {capacities.map((n) => (
-            <option value={n} key={n}>
-              {n}
-            </option>
-          ))}
-        </select>
-        <RenderErrors error={formErrors.capacity} />
-      </>
-    )}
+    <select
+      name='capacity'
+      onChange={onChange}
+      value={theme.capacity}
+      className={`${
+        formErrors.capacity ? 'border-2 border-red-500' : 'border border-gray-300'
+      } text-xl sm:text-2xl lg:text-3xl bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5`}
+    >
+      {capacities.map((n) => (
+        <option value={n} key={n}>
+          {n}
+        </option>
+      ))}
+    </select>
+    {formErrors.capacity ? <RenderErrors error={formErrors.capacity} /> : undefined}
   </>
 )
 
--- frontend/src/components/themes/titleForm.tsx
+++ frontend/src/components/themes/titleForm.tsx
@@ -9,32 +9,20 @@ interface Props {
 
 const TitleForm = ({ onChange, theme, formErrors }: Props) => (
   <>
-    {!formErrors.title ? (
-      <div className='relative z-0 p-4'>
-        <input
-          type='text'
-          id='themeTitleForm'
-          name='title'
-          onChange={onChange}
-          value={theme.title}
-          className='block py-2.5 px-0 w-full text-2xl sm:text-3xl lg:text-4xl text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-gray-400'
-          placeholder='ここにお題を入力'
-        />
-      </div>
-    ) : (
-      <div className='relative z-0 p-4'>
-        <input
-          type='text'
-          id='themeTitleForm'
-          name='title'
-          onChange={onChange}
-          value={theme.title}
-          className='block py-2.5 px-0 w-full text-2xl sm:text-3xl lg:text-4xl text-gray-900 bg-transparent border-0 border-b-2 border-red-500 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-gray-400'
-          placeholder='ここにお題を入力'
-        />
-        <RenderErrors error={formErrors.title} />
-      </div>
-    )}
+    <div className='relative z-0 p-4'>
+      <input
+        type='text'
+        id='themeTitleForm'
+        name='title'
+        onChange={onChange}
+        value={theme.title}
+        className={`${
+          formErrors.title ? 'border-red-500' : 'border-gray-300'
+        } block py-2.5 px-0 w-full text-2xl sm:text-3xl lg:text-4xl text-gray-900 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer placeholder-gray-400`}
+        placeholder='ここにお題を入力'
+      />
+      {formErrors.title ? <RenderErrors error={formErrors.title} /> : undefined}
+    </div>
   </>
 )
 
