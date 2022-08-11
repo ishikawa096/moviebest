c7d489747ba51f9a98cf8c687eb519b493759be6
ishikawa096 <ishikawa096@gmail.com>
2022-08-11 09:22:47 +0900

    繰り返し部分を省略、css調整

--- frontend/src/components/lists/postersContainer.tsx
+++ frontend/src/components/lists/postersContainer.tsx
@@ -1,45 +1,22 @@
 import { Movie } from 'interfaces/interface'
-import { toastWarn } from 'lib/toast'
-import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 
-const PostersContainer = ({ children, movies }: { children: ReactNode, movies: Array<Movie> }) => {
-  const router = useRouter()
-  const length =  movies.length
+const PostersContainer = ({ children, movies }: { children: ReactNode; movies: Array<Movie> }) => {
+  const length = movies.length
   if (length < 6) {
-    return <div className='mb-10 flex flex-row flex-no-wrap justify-center w-screen bg-black'>{children}</div>
-  }
-  if (length === 6) {
-    return (
-      <div className='mb-10 flex justify-center w-screen bg-black'>
-        <div className='grid grid-cols-3 gap-0 justify-center w-[fit-content]'>{children}</div>
-      </div>
-    )
-  }
-  if (length === 7 || length === 8) {
-    return (
-      <div className='mb-10 flex justify-center w-screen bg-black'>
-        <div className='grid grid-cols-4 gap-0 justify-center w-[fit-content]'>{children}</div>
-      </div>
-    )
-  }
-  if (length === 9) {
-    return (
-      <div className='mb-10 flex justify-center w-screen bg-black'>
-        <div className='grid grid-cols-3 sm:grid-cols-5 gap-0 justify-center w-[fit-content]'>{children}</div>
-      </div>
-    )
-  }
-  if (length === 10) {
-    return (
-      <div className='mb-10 flex justify-center w-screen bg-black'>
-        <div className='grid grid-cols-4 sm:grid-cols-5 gap-0 justify-center w-[fit-content]'>{children}</div>
-      </div>
-    )
+    return <div className='mb-10 flex flex-row flex-no-wrap justify-center w-screen bg-white'>{children}</div>
   } else {
-    toastWarn('エラーが起きました。前のページに戻ります')
-    router.back()
-    return <></>
+    return (
+      <div className='mb-10 flex justify-center w-screen bg-white'>
+        <div
+          className={`${
+            length === 6 ? 'grid-cols-3' : length === 7 || length === 8 ? 'grid-cols-4' : length === 9 ? 'grid-cols-3 sm:grid-cols-5' : 'grid-cols-4 sm:grid-cols-5'
+          } grid gap-0 justify-center w-[fit-content]`}
+        >
+          {children}
+        </div>
+      </div>
+    )
   }
 }
 
