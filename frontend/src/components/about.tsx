eabbe91397997855af58c8c192d3c19e5cdb6c44
ishikawa096 <ishikawa096@gmail.com>
2022-08-13 08:52:07 +0900

    firstView調整、aboutページ作成

---
+++ frontend/src/components/about.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+
+const About = () => (
+  <div className='relative py-5 px-10 md:px-28 lg:px-40 w-screen flex flex-col text-center items-center bg-yellow-400 text-gray-700'>
+    <h3 className='mb-10 text-3xl md:text-5xl font-bold text-white'>映画なんでもベストとは？</h3>
+    <p className='mb-3 text-lg'>自由なお題で映画ベストランキングを作って公開できるサービスです。</p>
+    <p className='mb-10 text-sm'>＊ お題やベストを作るにはログインが必要です。</p>
+    <h3 className='mb-5 text-3xl md:text-5xl font-bold text-white'>使い方</h3>
+    <div className='p-8 mb-10 bg-white w-full flex-col flex items-center gap-3 rounded-lg'>
+      <div className='rounded-lg overflow-hidden shadow-md w-[fit-content] bg-gray-100'>
+        <Image src='/screen.png' width={400} height={300} alt='作成画面' objectFit='contain' />
+      </div>
+      <div>投稿ボタンからベストを作成。お題を選んで映画を入力</div>
+    </div>
+    <div className='p-8 mb-10 bg-white w-full flex-col flex gap-3 rounded-lg'>
+      <div className='rounded-lg overflow-hidden'>
+        <Image src='/undraw_social_friends_re_7uaa.svg' width={400} height={300} alt='作成画面' objectFit='contain' />
+      </div>
+      <div>作ったベストはツイートボタンでツイートできます</div>
+    </div>
+    <div className='p-8 mb-10 bg-white w-full flex-col flex gap-3 rounded-lg'>
+      <div className='rounded-lg overflow-hidden'>
+        <Image src='/undraw_lightbulb_moment_re_ulyo_y.svg' width={400} height={300} alt='作成画面' objectFit='contain' />
+      </div>
+      <div>新しいお題を作って、自分好みの映画ベストを投稿しよう</div>
+    </div>
+  </div>
+)
+
+export default About
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,6 +5,8 @@ import ListsSlider from 'components/lists/listsSlider'
 import PopularThemes from 'components/themes/popularThemes'
 import LargeButton from 'components/commons/largeButton'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
+import About from 'components/about'
 
 interface Props {
   themes: Array<Theme & { lists: Array<List> }>
@@ -14,16 +16,23 @@ interface Props {
 const Home = (props: Props) => {
   const router = useRouter()
   const themes = props.themes
-  const lists = props.lists.sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1)).slice(0,11)
+  const lists = props.lists.sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1)).slice(0, 8)
   themes.sort((a, b) => b.lists.length - a.lists.length)
 
   return (
     <>
       <PageHead />
       <section>
-        <div className='mb-10 w-screen text-center bg-gradient-to-r from-yellow-300 to-orange-400 py-40'>
-          <h1 className='mb-10 font-logo text-3xl'>映画なんでもベスト</h1>
-          <h3 className='font-light'>お題に合わせて自分だけの映画ベストランキングを作ろう</h3>
+        <div className='relative justify-center mb-10 w-screen h-[80vh] text-center'>
+          <div className='text-[0px]'>
+            <Image src={'/Kander.svg'} alt='背景画像' priority={true} layout='fill' objectFit='cover' />
+          </div>
+          <div className='p-5 flex flex-col justify-center text-center items-center bg-white absolute top-[30%] bottom-[30%] right-0 left-0 md:top-0 md:bottom-0 md:right-[30%] md:left-[30%] min-w-[10rem] z-10'>
+            <h1 className='mb-10 font-logo text-3xl whitespace-nowrap'>
+              映画<span className='text-2xl'>なんでも</span>ベスト
+            </h1>
+            <h3 className='font-light'>お題に合わせて自分だけの映画ベストランキングを作ろう</h3>
+          </div>
         </div>
       </section>
       <section className='w-screen'>
@@ -33,7 +42,7 @@ const Home = (props: Props) => {
           </div>
           <ListsSlider lists={lists} />
         </div>
-        <div className='mb-10 flex justify-center'>
+        <div className='mb-32 flex justify-center'>
           <LargeButton
             title='新着をもっと見る'
             onClick={() => {
@@ -49,14 +58,13 @@ const Home = (props: Props) => {
           </div>
           <PopularThemes themes={themes} />
         </div>
-        <div className='mb-10 flex flex-row items-center text-center justify-around gap-10'>
+        <div className='mb-32 flex flex-row items-center text-center justify-around gap-10'>
           <LargeButton
             title='お題をもっと見る'
             onClick={() => {
               router.push('/themes')
             }}
           />
-
           <LargeButton
             title='お題をつくる'
             onClick={() => {
@@ -65,8 +73,27 @@ const Home = (props: Props) => {
           />
         </div>
       </section>
+      <section id='about'>
+        <About />
+      </section>
       <section>
-        <h3 className='font-black text-transparent text-3xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>映画なんでもベストとは？</h3>
+        <div className='py-20 my-10 mx-30 py-5 px-10  md:px-28 lg:px-40 w-full flex flex-col text-center items-center bg-white text-gray-700'>
+          <h3 className='mb-10 text-3xl font-bold'>はじめよう</h3>
+          <div className='flex flex-col md:flex-row gap-5'>
+            <LargeButton
+              title='ログイン'
+              onClick={() => {
+                router.push('/signin')
+              }}
+            />
+            <LargeButton
+              title='新規登録'
+              onClick={() => {
+                router.push('/signup')
+              }}
+            />
+          </div>
+        </div>
       </section>
     </>
   )
