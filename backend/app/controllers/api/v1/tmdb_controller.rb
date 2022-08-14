40885ae682ce8882b1ef42c7a53c87a65739f6bb
ishikawa096 <ishikawa096@gmail.com>
2022-08-14 13:50:45 +0900

    未使用の外部apiを一旦削除

--- backend/app/controllers/api/v1/tmdb_controller.rb
+++ backend/app/controllers/api/v1/tmdb_controller.rb
@@ -7,13 +7,4 @@ class Api::V1::TmdbController < ApplicationController
     response = Tmdb::SearchClient.get(params[:keyword])
     render json: response.body, status: response.code
   end
-
-  def images
-    if params[:tmdb_id].blank?
-      render json: { status: 400, message: 'TMDB ID is requred' }
-      return
-    end
-    response = Tmdb::ImagesClient.get(params[:tmdb_id])
-    render json: response.body, status: response.code
-  end
 end
