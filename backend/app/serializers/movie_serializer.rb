35c924f43d93b2672c1a72a54a971cc8c39a80a3
ishikawa096 <ishikawa096@gmail.com>
2022-08-14 13:28:13 +0900

    不使用のためjsonapi-serializer削除

--- backend/app/serializers/movie_serializer.rb
+++
@@ -1,6 +0,0 @@
-class MovieSerializer
-  include JSONAPI::Serializer
-  set_key_transform :camel_lower
-
-  attributes :id, :title, :position, :list_id, :created_at, :updated_at
-end
--- backend/app/serializers/theme_serializer.rb
+++
@@ -1,6 +0,0 @@
-class ThemeSerializer
-  include JSONAPI::Serializer
-  set_key_transform :camel_lower
-
-  attributes :id, :title, :capacity, :created_at, :updated_at
-end
