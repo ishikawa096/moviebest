8699fe2e68dfbd6e137484d27cbf5fa7401e65ef
ishikawa096 <ishikawa096@gmail.com>
2022-08-15 18:39:23 +0900

    firstView等デザイン変更

--- frontend/src/components/about.tsx
+++ frontend/src/components/about.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 const About = () => (
-  <div className='relative py-5 px-10 md:px-28 lg:px-40 w-screen flex flex-col text-center items-center bg-yellow-400 text-gray-700'>
+  <div className='relative py-5 px-10 md:px-28 lg:px-40 w-screen flex flex-col text-center items-center text-gray-600 bg-gradient-to-br from-cyan-400 via-sky-300 to-cyan-300'>
     <h3 className='mb-10 text-3xl md:text-5xl font-bold text-white'>映画なんでもベストとは？</h3>
     <p className='mb-3 text-lg'>自由なお題で映画ベストランキングを作って公開できるサービスです。</p>
     <p className='mb-10 text-sm'>＊ お題やベストを作るにはログインが必要です。</p>
@@ -16,7 +16,7 @@ const About = () => (
       <div className='rounded-lg overflow-hidden'>
         <Image src='/undraw_social_friends_re_7uaa.svg' width={400} height={300} alt='作成画面' objectFit='contain' />
       </div>
-      <div>作ったベストはツイートボタンでツイートできます</div>
+      <div>作ったベストはツイートボタンでシェアできます<br />「マイベスト」から編集も可能</div>
     </div>
     <div className='p-8 mb-10 bg-white w-full flex-col flex gap-3 rounded-lg'>
       <div className='rounded-lg overflow-hidden'>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -29,12 +29,15 @@ const Home = (props: Props) => {
           <div className='text-[0px] bg-white'>
             <Image src={'/Kander.svg'} alt='背景画像' priority={true} layout='fill' objectFit='cover' />
           </div>
-          <div className='p-5 flex flex-col justify-center text-center items-center bg-white absolute top-[30%] bottom-[30%] right-0 left-0 md:top-0 md:bottom-0 md:right-[30%] md:left-[30%] min-w-[10rem] z-10'>
-            <h1 className='mb-10 font-logo text-3xl whitespace-nowrap motion-safe:animate-fadeIn'>
-              映画<span className='text-2xl'>なんでも</span>ベスト
+          <div className='p-5 flex flex-col justify-center text-center items-center bg-white absolute top-1/2 bottom-1/2 right-1/3 left-1/3 z-10'>
+            <h1 className='mb-10 font-logo text-3xl md:text-4xl bg-white whitespace-nowrap px-20 py-14 lg:px-32 lg:py-16 drop-shadow-[30px_30px_0px_rgba(0,30,50,0.50)] motion-safe:animate-fadeIn'>
+              映画<span className='text-2xl md:text-3xl'>なんでも</span>ベスト
             </h1>
-            <h3 className='font-light'>お題に合わせて自分だけの映画ベストランキングを作ろう</h3>
           </div>
+          <h3 className='absolute font-light italic top-3/4 left-1/4 md:left-1/3 right-5 z-10 bg-white px-5 py-3 md:px-10 md:py-5 drop-shadow-[30px_30px_0px_rgba(0,30,50,0.50)]'>
+            お題 に合わせて
+            <span className='underline decoration-orange-500 underline-offset-4 decoration-2 decoration-wavy'>自分だけの映画ベストランキング</span>を作ろう
+          </h3>
         </div>
       </section>
       <section className='w-screen'>
@@ -81,21 +84,26 @@ const Home = (props: Props) => {
         <About />
       </section>
       <section>
-        <div className='py-20 my-10 mx-30 py-5 px-10  md:px-28 lg:px-40 w-full flex flex-col text-center items-center bg-white text-gray-700'>
-          <h3 className='mb-10 text-3xl font-bold'>はじめよう</h3>
-          <div className='flex flex-col md:flex-row gap-5'>
-            <LargeButton
-              title='ログイン'
-              onClick={() => {
-                router.push('/signin')
-              }}
-            />
-            <LargeButton
-              title='新規登録'
-              onClick={() => {
-                router.push('/signup')
-              }}
-            />
+        <div className='relative min-h-[28rem] py-20 mx-30 py-5 px-10  md:px-28 lg:px-40 w-full flex flex-col text-center items-center bg-white text-gray-700'>
+          <div className='text-[0px] bg-white'>
+            <Image src={'/Kander.svg'} alt='背景画像' priority={true} layout='fill' objectFit='cover' />
+          </div>
+          <div className='p-5 flex flex-col justify-center text-center items-center absolute top-1/2 bottom-1/2 z-10'>
+            <h3 className='mb-10 text-3xl font-light bg-white italic  px-10 py-7 drop-shadow-[30px_30px_0px_rgba(0,30,50,0.50)] '>はじめよう</h3>
+            <div className='flex flex-col md:flex-row gap-10 md:gap-5 drop-shadow-[30px_30px_0px_rgba(0,30,50,0.50)]'>
+              <LargeButton
+                title='ログイン'
+                onClick={() => {
+                  router.push('/signin')
+                }}
+              />
+              <LargeButton
+                title='新規登録'
+                onClick={() => {
+                  router.push('/signup')
+                }}
+              />
+            </div>
           </div>
         </div>
       </section>
