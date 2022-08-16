18eb87dcb1cc35a2aa3355bf625c6952283546a1
ishikawa096 <ishikawa096@gmail.com>
2022-08-16 13:48:35 +0900

    スタイル調整

--- frontend/src/components/commons/tweetButton.tsx
+++ frontend/src/components/commons/tweetButton.tsx
@@ -11,7 +11,8 @@ interface Props {
 const TweetButton = ({ themeTitle, movies }: Props) => (
   <TweetWrapper themeTitle={themeTitle} movies={movies}>
     <div className='block w-32 text-center rounded-lg px-4 py-2 font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 whitespace-nowrap bg-sky-400 hover:bg-sky-500'>
-      <FontAwesomeIcon icon={faTwitter} /> tweet
+      <FontAwesomeIcon icon={faTwitter} className='pr-1' />
+      tweet
     </div>
   </TweetWrapper>
 )
--- frontend/src/components/renderErrors.tsx
+++ frontend/src/components/renderErrors.tsx
@@ -3,8 +3,8 @@ import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 
 const RenderErrors = ({ error }: { error: string }) => {
   return (
-    <div className='w-full p-2 text-sm text-red-500'>
-      <FontAwesomeIcon icon={faTriangleExclamation} />
+    <div className='w-full p-2 text-sm text-red-500 motion-safe:animate-fade'>
+      <FontAwesomeIcon icon={faTriangleExclamation} className='px-1' />
       {error}
     </div>
   )
