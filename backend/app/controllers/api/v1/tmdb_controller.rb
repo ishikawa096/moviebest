6753ba15802b1ab9be69e0dec819220289231aee
ishikawa096 <ishikawa096@gmail.com>
2022-08-16 18:15:07 +0900

    外部APIからのエラーハンドリング追加

--- backend/app/controllers/api/v1/tmdb_controller.rb
+++ backend/app/controllers/api/v1/tmdb_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::TmdbController < ApplicationController
   def search
     if params[:keyword].blank?
-      render json: { status: 400, message: 'Keyword is requred' }
+      render json: { message: 'Keyword is requred' }, status: :bad_request
       return
     end
     response = Tmdb::SearchClient.get(params[:keyword])
-    render json: response.body, status: response.code
+    if response.code == '200'
+      render json: response.body, status: :ok
+    else
+      message = JSON.parse(response.body)['status_message']
+      render json: { message: "API error (#{message})" },
+        status: response.code.to_i
+    end
   end
 end
