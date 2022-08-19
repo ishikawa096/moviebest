db68d697155bb2b00baf6417e75e0ad2cec748b2
ishikawa096 <ishikawa096@gmail.com>
2022-08-19 12:50:53 +0900

    [fix]users#showでid,nameのみのuserデータを返すよう修正
    セキュリティのため

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def show
     user = User.includes(lists: %i[theme movies]).find(params[:id])
-    json = user.as_json(include: { lists: { include: %i[theme movies] } })
+    json = user.as_json(only: %i[name id], include: { lists: { include: %i[theme movies] } })
     render json:, status: :ok
   end
 end
