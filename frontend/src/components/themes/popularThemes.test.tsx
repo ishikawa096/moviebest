d954fcc9b886901945f12b7c49c94517ca4e8e54
ishikawa096 <ishikawa096@gmail.com>
2022-08-20 13:46:52 +0900

    [update]theme一覧でクリック後ミニメニューが出る機能追加
    作成と詳細表示の両方に移動できるようにするため

--- frontend/src/components/themes/popularThemes.test.tsx
+++ frontend/src/components/themes/popularThemes.test.tsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import PopularThemes from './popularThemes'
 import { themeMock } from 'mocks/mockData'
+import userEvent from '@testing-library/user-event'
 
 jest.mock('components/layout/styles', () => ({
   randomBackGroundColors: ['bg-yellow-300'],
@@ -19,8 +20,20 @@ describe('PopularThemes', () => {
     expect(container.innerHTML).toMatch('0')
   })
 
-  test('ランダムな背景色があること', async () => {
-    const { container } = render(<PopularThemes themes={themes} />)
-    expect(container.innerHTML).toMatch('bg-yellow-300')
+  test('クリックで一覧と作成へのリンクが出ること', async () => {
+    const user = userEvent.setup()
+    render(<PopularThemes themes={themes} />)
+    const card = screen.getByText('THEME')
+    await user.click(card)
+    expect(screen.getByText(/このお題でつくる/).closest('div')).toHaveClass('block')
+    expect(screen.getByText(/投稿を見る/).closest('div')).toHaveClass('block')
+  })
+
+  test('デフォルトではリンクは非表示', async () => {
+    render(<PopularThemes themes={themes} />)
+    const createLink = screen.getByText(/このお題でつくる/)
+    const showLink = screen.getByText(/投稿を見る/)
+    expect(showLink.closest('div')).toHaveClass('hidden')
+    expect(createLink.closest('div')).toHaveClass('hidden')
   })
 })
--- frontend/src/components/themes/popularThemes.tsx
+++ frontend/src/components/themes/popularThemes.tsx
@@ -2,35 +2,54 @@ import { randomBackGroundColors } from 'components/layout/styles'
 import { List, Theme } from 'interfaces/interface'
 import { arrayRandom } from 'lib/helpers'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 interface Props {
   themes: Array<Theme & { lists: Array<List> }>
 }
 
-const ThemeCard = ({ theme }: { theme: Theme & { lists: Array<List> } }) => (
+const ThemeCard = ({ theme, selected, onClick, color }: { theme: Theme & { lists: Array<List> }; selected: any; onClick: any; color: string }) => (
   <div
-    className={`flex items-center h-auto px-3 py-1 hover:text-yellow-400 hover:bg-white hover:shadow-sm rounded-md duration-150 ease-in-out ${
+    onClick={onClick}
+    className={`${selected === theme.id ? 'text-yellow-400 bg-white' : color} ${
       theme.lists.length > 3 ? 'text-2xl md:text-4xl' : 'text-base md:text-2xl'
-    } ${arrayRandom(randomBackGroundColors)}`}
+    } relative flex items-center font-medium px-3 py-1 hover:text-yellow-400 hover:bg-white hover:shadow-sm cursor-pointer rounded-md duration-150 ease-in-out`}
   >
-    <Link href={`/themes/${theme.id}`}>
-      <a className='font-bold'>
-        {theme.title}
-        <span className='rounded-full text-sm md:text-xl'>／{theme.lists.length}</span>
-      </a>
-    </Link>
+    {theme.title}
+    <span className='rounded-full text-sm md:text-xl'>／{theme.lists.length}</span>
+
+    <div
+      className={`${
+        selected === theme.id ? 'block' : 'hidden'
+      } absolute top-10 min-w-[10rem] md:min-w-[12rem] flex flex-col bg-white text-center text-base md:text-lg text-sky-600 rounded-lg border z-10 motion-safe:animate-zoomIn`}
+    >
+      <Link href={`/lists/new?id=${theme.id}`}>
+        <a className='block hover:text-sky-500 hover:bg-sky-100 p-2 cursor-pointer border-b'>このお題でつくる</a>
+      </Link>
+      <Link href={`/themes/${theme.id}`}>
+        <a className='block hover:text-sky-500 hover:bg-sky-100 p-2 cursor-pointer'>投稿を見る</a>
+      </Link>
+    </div>
   </div>
 )
 
 const PopularThemes = ({ themes }: Props) => {
+  const [selected, setSelected] = useState<number | undefined>(undefined)
+  const [colors, setColors] = useState<Array<string>>([])
+
+  useEffect(() => {
+    const colorArray = [...Array(themes.length)].fill(null).map((_, i) => {
+      return arrayRandom(randomBackGroundColors) as string
+    })
+    setColors(colorArray)
+  }, [])
+
   return (
-    <>
-      <div className='flex px-3 md:px-10 flex-row flex-wrap justify-center gap-3'>
-        {themes.map((theme) => (
-          <ThemeCard key={theme.id} theme={theme} />
-        ))}
-      </div>
-    </>
+    <div className='flex px-3 md:px-10 flex-row flex-wrap justify-center gap-3'>
+      {themes.map((theme, i) => (
+        <ThemeCard key={theme.id} theme={theme} selected={selected} onClick={() => (selected === theme.id ? setSelected(undefined) : setSelected(theme.id))} color={colors[i]} />
+      ))}
+    </div>
   )
 }
 
