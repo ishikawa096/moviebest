0537b41ca7587cf15f8cb04e8c842ae56368fb1e
ishikawa096 <ishikawa096@gmail.com>
2022-08-24 20:19:59 +0900

    [change]スマホ閲覧時ポスター画像3つで改行するよう変更
    4,5つだと縮小され画像が小さいため

--- frontend/src/components/lists/posters.tsx
+++ frontend/src/components/lists/posters.tsx
@@ -7,6 +7,8 @@ import { faHashtag } from '@fortawesome/free-solid-svg-icons'
 import { setImageUrl } from 'lib/tmdbHelpers'
 import Poster from './poster'
 
+const BLUR_IMAGE = '/asset/image/342x509.png'
+
 const Posters = ({ movies, theme, blankImage }: { movies: Array<Movie>; theme: Theme; blankImage: string }) => {
   const [update, setUpdate] = useState(false)
   const [firstMovieImage, setFirstMovieImage] = useState(setImageUrl(movies[0]))
@@ -30,16 +32,16 @@ const Posters = ({ movies, theme, blankImage }: { movies: Array<Movie>; theme: T
   return (
     <>
       {movies.map((movie) => (
-        <Poster key={movie.id} movie={movie} blankImage={blankImage} />
+        <Poster key={'poster-' + movie.id} movie={movie} blankImage={blankImage} />
       ))}
       {
         // グリッドに空ができる場合、埋める
-        movies.length === 7 || (movies.length === 9 && isWideWindow) ? (
-          <div className='relative flex justify-center overflow-hidden'>
+        (movies.length === 5 && !isWideWindow) || movies.length === 7 || (movies.length === 8 && !isWideWindow) || (movies.length === 9 && isWideWindow) || (movies.length === 10 && !isWideWindow) ? (
+          <div className={`${(movies.length === 7 && !isWideWindow) || movies.length === 10 ? 'col-span-2' : ''} relative flex justify-center overflow-hidden`}>
             <div className='text-[0px]'>
-              <Image src={firstMovieImage} alt={theme.title} layout='fill' objectFit='cover' placeholder='blur' blurDataURL='/342x509.png' onError={() => onError()} />
+              <Image src={firstMovieImage} alt={theme.title} layout='fill' objectFit='cover' placeholder='blur' blurDataURL={BLUR_IMAGE} onError={() => onError()} />
 
-              <div className='p-5 text-base text-shadow flex flex-col justify-center text-center items-center top-0 left-0 right-0 bottom-0 absolute z-10 backdrop-blur-xl text-white'>
+              <div className='p-5 text-base text-shadow flex flex-row justify-center text-center items-center top-0 left-0 right-0 bottom-0 absolute z-10 backdrop-blur-xl text-white'>
                 <FontAwesomeIcon icon={faHashtag} size='sm' className='px-1' />
                 {theme.title}
               </div>
@@ -47,19 +49,6 @@ const Posters = ({ movies, theme, blankImage }: { movies: Array<Movie>; theme: T
           </div>
         ) : null
       }
-
-      {movies.length === 10 && !isWideWindow ? (
-        <div className='col-span-2 relative flex justify-center overflow-hidden'>
-          <div className='text-[0px]'>
-            <Image src={firstMovieImage} alt={theme.title} layout='fill' objectFit='cover' placeholder='blur' blurDataURL='/342x509.png' onError={() => onError()} />
-
-            <div className='p-5 text-base text-shadow flex flex-col justify-center text-center items-center top-0 left-0 right-0 bottom-0 absolute z-10 backdrop-blur-xl text-white'>
-              <FontAwesomeIcon icon={faHashtag} size='sm' className='px-1' />
-              {theme.title}
-            </div>
-          </div>
-        </div>
-      ) : null}
     </>
   )
 }
--- frontend/src/components/lists/postersContainer.tsx
+++ frontend/src/components/lists/postersContainer.tsx
@@ -3,15 +3,17 @@ import { ReactElement } from 'react'
 
 const PostersContainer = ({ children, movies }: { children: ReactElement; movies: Array<Movie> }) => {
   const length = movies.length
-  if (length < 6) {
+  if (length < 4) {
     return <div className='mb-10 flex flex-row flex-no-wrap justify-center w-screen bg-gray-100'>{children}</div>
   } else {
     return (
       <div className='mb-10 flex justify-center w-screen bg-gray-100'>
         <div
-          className={`${
-            length === 6 ? 'grid-cols-3' : length === 7 || length === 8 ? 'grid-cols-4' : length === 9 ? 'grid-cols-3 sm:grid-cols-5' : 'grid-cols-4 sm:grid-cols-5'
-          } grid gap-0 justify-center w-[fit-content]`}
+          className={`
+          ${length === 4 ? 'grid-cols-2 sm:grid-cols-4' : 'grid-cols-3'}
+            ${
+              length === 6 ? '' : length === 7 || length === 8 ? 'sm:grid-cols-4' : 'sm:grid-cols-5'
+            } grid gap-0 justify-center w-[fit-content]`}
         >
           {children}
         </div>
