745f51e67245c70f8804dd6423f44ed89e66aead
ishikawa096 <ishikawa096@gmail.com>
2022-09-08 15:57:15 +0900

    [update]スライダー上のCard削除時に画面リロード
    スライダー上でも削除できるようになったため機能追加

--- frontend/src/components/lists/listsSlider.test.tsx
+++ frontend/src/components/lists/listsSlider.test.tsx
@@ -1,19 +1,58 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import ListsSlider from './listsSlider'
 import { listMock, themeMock, userMock } from 'mocks/mockData'
-import { Movie, Theme, User } from 'interfaces/interface'
+import userEvent from '@testing-library/user-event'
+import { AuthContext } from 'pages/_app'
+import { server } from 'mocks/server'
 
 const lists = [{ ...listMock, theme: themeMock, user: userMock }]
+const user = userEvent.setup()
 
-jest.mock('./listsSlider', () => {
-  const MockListCard = ({ theme: theme, user: user, movies: movies }: { theme: Theme; user: User; movies: Array<Movie> }) => <div data-testid='list-card'></div>
-  return MockListCard
-})
+const mockedReload = jest.fn()
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      isReady: true,
+      reload: mockedReload,
+    }
+  },
+}))
 
 describe('ListsSlider', () => {
-  test('listCardが表示されること', async () => {
-    render(<ListsSlider lists={lists} />)
-    expect(screen.getByTestId('list-card')).toBeTruthy
+  beforeAll(() => {
+    server.listen()
+  })
+  afterAll(() => {
+    server.close()
+  })
+  beforeEach(() => {
+    render(
+      <AuthContext.Provider
+        value={{
+          loading: false,
+          setLoading: jest.fn(),
+          isSignedIn: true,
+          setIsSignedIn: jest.fn(),
+          currentUser: userMock,
+          setCurrentUser: jest.fn(),
+          isGuest: false,
+          setIsGuest: jest.fn(),
+        }}
+      >
+        <ListsSlider lists={lists} />
+      </AuthContext.Provider>
+    )
+  })
+
+  test('titleやuser名が表示されること', async () => {
+    expect(screen.getByText(themeMock.title)).toBeInTheDocument
+    expect(screen.getByText(userMock.name)).toBeInTheDocument
+  })
+
+  test('listCardを削除するとrouter.reloadが呼ばれること', async () => {
+    await waitFor(() => user.click(screen.getByRole('button', { name: /削除/ })))
+    await waitFor(() => user.click(screen.getByText('削除する')))
+    await waitFor(() => expect(mockedReload).toBeCalledTimes(1))
   })
 })
--- frontend/src/components/lists/listsSlider.tsx
+++ frontend/src/components/lists/listsSlider.tsx
@@ -5,62 +5,65 @@ import { List, Theme, User } from 'interfaces/interface'
 import ListCard from './listCard'
 import { useMemo } from 'react'
 import React from 'react'
+import { useRouter } from 'next/router'
 
 interface Props {
   lists: Array<List & { theme: Theme; user: User }>
 }
 
 const ListsSlider = React.memo(({ lists }: Props) => {
+  const router = useRouter()
   const listsData = useMemo(() => lists, [lists])
 
-  const settings = useMemo(()=> ({
-    dots: false,
-    arrows: false,
-    infinite: true,
-    autoplay: true,
-    centerMode: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
-    centerPadding: '35%',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    cssEase: 'linear',
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          centerPadding: '30%',
+  const settings = useMemo(
+    () => ({
+      dots: false,
+      arrows: false,
+      infinite: true,
+      autoplay: true,
+      centerMode: true,
+      speed: 2000,
+      autoplaySpeed: 2000,
+      centerPadding: '35%',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            centerPadding: '30%',
+          },
         },
-      },
-      {
-        breakpoint: 1000,
-        settings: {
-          centerPadding: '20%',
+        {
+          breakpoint: 1000,
+          settings: {
+            centerPadding: '20%',
+          },
         },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          centerPadding: '15%',
+        {
+          breakpoint: 768,
+          settings: {
+            centerPadding: '15%',
+          },
         },
-      },
-      {
-        breakpoint: 585,
-        settings: {
-          centerMode: false,
+        {
+          breakpoint: 585,
+          settings: {
+            centerMode: false,
+          },
         },
-      },
-    ],
-  }), [])
+      ],
+    }),
+    []
+  )
 
   return (
-    <div className='overflow-hidden w-full'>
-      <Slider {...settings} className='relative'>
-        {listsData.map((list) => (
-          <ListCard key={'list-slider-' + list.id} user={list.user} movies={list.movies} theme={list.theme} />
-        ))}
-      </Slider>
-    </div>
+    <Slider {...settings}>
+      {listsData.map((list) => (
+        <ListCard key={'list-slider-' + list.id} user={list.user} movies={list.movies} theme={list.theme} onDelete={() => router.reload()} />
+      ))}
+    </Slider>
   )
 })
 
