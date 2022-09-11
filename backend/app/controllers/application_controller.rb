469dae48f7de40711261ba4319b9aefa7f7aaada
ishikawa096 <ishikawa096@gmail.com>
2022-09-12 00:25:12 +0900

    [clean]コントローラーのリファクタリング
    不要な記述削除、共通部切り出し

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::API
-  include ActionController::Helpers
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
-  helper_method :current_user, :user_signed_in?, :authenticate_user!, :ensure_not_guest_user
   before_action :authenticate
 
   private
